c67a42e5b24dd037fb357218471917bd8391b5b2
danny-for <dannygeovanna2019@gmail.com>
2022-06-15 16:24:21 -0300

    @Danny-for[Task: update {15/06/2022 16:24}]

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/estaticos/footer/Footer';
-import Footer from './components/estaticos/navbar/Navbar';
+import Navbar from './components/estaticos/navbar/Navbar';
+import Footer from './components/estaticos/footer/Footer';
 import './App.css';
 import Home from './paginas/home/Home';
 import Login from './paginas/login/Login';
